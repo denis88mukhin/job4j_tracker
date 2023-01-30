3d26cee582069711d8adf6ea66b4e44d1d268602
Denis Mukhin <mukhin.denis88@gmail.com>
2023-01-30 12:36:34 +0300

    3. Массивы и модели. [#174892 #386255]

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -27,7 +27,7 @@ public class Library {
         System.out.println("Show only books.name == \"Clean code\"");
         for (int index = 0; index < books.length; index++) {
             Book book = books[index];
-            if (book.getName() == "Clean Code") {
+            if ("Clean Code".equals(book.getName())) {
                 System.out.println(book.getName() + " - " + book.getPages() + " pages.");
             }
         }
