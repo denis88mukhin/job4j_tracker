23493de734e7b46e11e5e25ce72eb843db67f821
Denis Mukhin <mukhin.denis88@gmail.com>
2023-01-31 20:44:23 +0300

    10. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,5 +1,6 @@
 package ru.job4j.tracker;
 
+import javax.sound.midi.Soundbank;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -11,5 +12,8 @@ public class StartUI {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String createdDateTimeFormat = createdDateTime.format(formatter);
         System.out.println(createdDateTimeFormat);
+
+        Item item1 = new Item();
+        System.out.println(item1);
     }
 }
