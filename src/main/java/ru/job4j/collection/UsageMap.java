6671326b628d962013f5f9c299f520bfa1b4d704
Denis Mukhin <mukhin.denis88@gmail.com>
2024-02-26 19:24:51 +0300

    2. Преобразования List в Map. [#10093]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -5,7 +5,10 @@ import java.util.HashMap;
 public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
-        map.put("mukhin.denis88@gmail.com", "Mukhin Denis Mikhaylovich");
+        map.put("mukhin.denis@gmail.com", "Mukhin Denis Mikhaylovich");
+        map.put("parsentev@yandex.ru", "Petr Arsentev");
+        map.put("mukhin.denis@gmail.com", "Mukhin Denis");
+        map.put("ivanov@gmail.com", "Ivanov Ivan");
         for (String key : map.keySet()) {
             System.out.println(map.get(key));
         }
