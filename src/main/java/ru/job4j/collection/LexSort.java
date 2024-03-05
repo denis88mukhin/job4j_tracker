b7c46d42434cd8d0b6ddeebdded7c0bc1a9b1bd6
Denis Mukhin <mukhin.denis88@gmail.com>
2024-03-05 20:10:41 +0300

    4. Сортировка номера [#221284]

--- src/main/java/ru/job4j/collection/LexSort.java
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -5,7 +5,7 @@ import java.util.Comparator;
 public class LexSort implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        return Integer.compare(Integer.parseInt(left.split("\\p{Punct}")[0]),
-                                Integer.parseInt(right.split("\\p{Punct}")[0]));
+        return Integer.compare(Integer.parseInt(left.split("\\.")[0]),
+                                Integer.parseInt(right.split("\\.")[0]));
     }
 }
